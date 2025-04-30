2a3ad29637eefe888d672d2c92d9e8ba4650d6bc
Hamza Aitaabella <haitaabe@e1r1p6.1337.ma>
2025-04-30 17:12:27 +0100

    new stuff

--- parsing_part/parsing.h
+++ parsing_part/parsing.h
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 13:45:31 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/04/30 16:26:58 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/04/30 17:05:04 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include<readline/readline.h>
-#include<readline/history.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <limits.h>
 #include "../libft/libft.h"
 
@@ -47,6 +47,8 @@ typedef struct s_word
     struct s_word *next;
 } t_word;
 
-int chexk_is_space(int check);
+int check_is_space(int check_s);
+int skip_spaces(char *str, int i);
+int skip_quotes(char *str, int i);
 
 #endif
--- parsing_part/utils_func.c
+++ parsing_part/utils_func.c
@@ -6,13 +6,36 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 13:55:09 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/04/30 15:58:21 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/04/30 17:04:19 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-int chexk_is_space(int check)
+int check_is_space(int check_s)
 {
-    return (check == 32 || (check >= 9 && check <= 13));
+    return (check_s == 32 || (check_s >= 9 && check_s <= 13));
+}
+
+
+int skip_spaces(char *str, int i)
+{
+    while (str[i] && check_is_space(str[i]))
+        i++;
+    return i;
+}
+int skip_quotes(char *str, int i)
+{
+    char quote;
+    if (str[i] == '\'' || str[i] == '"')
+    {
+        quote = str[i];
+        i++;
+        while (str[i] && str[i] != quote)
+            i++;
+        if (str[i] == quote)
+            i++;
+    }
+
+    return i;
 }

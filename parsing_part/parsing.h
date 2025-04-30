2aafae533ca39415d147b930c3d1d3fc6374fa6b
Hamza Aitaabella <haitaabe@e1r1p6.1337.ma>
2025-04-30 13:47:21 +0100

    setting up the envirement for our work

---
+++ parsing_part/parsing.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 13:45:31 by haitaabe          #+#    #+#             */
+/*   Updated: 2025/04/30 13:46:39 by haitaabe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+#define PARSING_H
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+
+
+
+
+
+#endif

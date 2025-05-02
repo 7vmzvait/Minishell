b36ef1c0ce3747f9a1446dbde3dcf26ec63c3394
Hamza Aitaabella <haitaabe@e1r2p4.1337.ma>
2025-05-02 11:38:08 +0100

    its just a test

--- parsing_part/utils_func.c
+++ parsing_part/utils_func.c
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 13:55:09 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/04/30 17:15:17 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/05/02 11:35:29 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,17 @@ int skip_quotes(char *str, int i)
 
 char **tokenize(char *line)
 {
-    
+   int i = 0;
+   t_word *save_tokens= (t_word *)malloc(sizeof(t_word));
+   while (line[i] != '\0')
+   {
+        if (check_is_space(line[i]))
+            skip_spaces(line, i);
+        else if (check_d_q(line[i]) || check_s_q(line[i]))
+            skip_quotes(line, i);
+        else if (check_types(&line[i]))
+            check_types(line);
+   }
+   line[i] = '\0';
+   return (line);
 }

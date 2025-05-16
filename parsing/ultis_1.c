8f6a8e0dee3615b75c6cff0b0900fb7dcad5f20d
Hamza Aitaabella <haitaabe@e1r1p4.1337.ma>
2025-05-16 12:06:53 +0100

    a small update

--- parsing/ultis_1.c
+++ parsing/ultis_1.c
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:49:56 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/05/16 11:53:47 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/05/16 12:06:40 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int is_operator(char c)
 {
     return (c == '|' || c == '<' || c == '>');
 }
-
+// to fill the struct 
 t_cmd *parse_tokens(t_token *tokens)
 {
     t_cmd *cmd_list = NULL;

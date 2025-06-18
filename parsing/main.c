a48e18c910ce2ddcaf513285967bc7004de27f99
Hamza Aitaabella <haitaabe@e1r1p10.1337.ma>
2025-06-18 11:26:14 +0100

    an update

--- parsing/main.c
+++ parsing/main.c
@@ -6,17 +6,12 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 20:15:32 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/06/17 20:15:47 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/06/18 09:37:28 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
-int main() {
-    char *line = "ls -l | grep txt > output.txt";
-    char **tokens = tokenize_input(line);
-
-    for (int i = 0; tokens[i]; i++)
-        printf("token[%d]: %s\n", i, tokens[i]);
-
-    // Don’t forget to free tokens later
+int main()
+{
+    
 }
--- parsing/utils_8.c
+++ parsing/utils_8.c
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 17:30:31 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/06/17 18:18:21 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/06/17 20:30:02 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,37 @@
 
 t_cmd *parse_tokens(char **tokens)
 {
+    t_cmd *head = init_command();
+    t_cmd *current = head;
     int i = 0;
-    t_cmd *head = NULL;
-    t_cmd *current = NULL;
 
     while (tokens[i])
     {
-        if (!current)
+        if (ft_strcmp(tokens[i], "<") == 0)
         {
-            current = new_cmd();
-            if (!head)
-                head = current;
-        }
-
-        if (!ft_strcmp(tokens[i], "|"))
-        {
-            current->next = new_cmd();
-            current = current->next;
+            i++;
+            current->infile = ft_strdup(tokens[i]);
             i++;
         }
-        else if (!ft_strcmp(tokens[i], "<") || !ft_strcmp(tokens[i], ">") ||
-                 !ft_strcmp(tokens[i], ">>") || !ft_strcmp(tokens[i], "<<"))
+        else if (ft_strcmp(tokens[i], ">") == 0)
         {
-            handle_redir(current, tokens, &i);
+            i++;
+            current->outfile = ft_strdup(tokens[i]);
+            current->append = 0;
+            i++;
+        }
+        else if (ft_strcmp(tokens[i], ">>") == 0)
+        {
+            i++;
+            current->outfile = ft_strdup(tokens[i]);
+            current->append = 1;
+            i++;
+        }
+        else if (ft_strcmp(tokens[i], "|") == 0)
+        {
+            t_cmd *new_cmd = init_command();
+            current->next = new_cmd;
+            current = new_cmd;
             i++;
         }
         else

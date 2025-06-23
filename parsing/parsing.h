134e741b5ac21fd3ce789bc69d0afb851280c30a
Hamza Aitaabella <haitaabe@e2r13p7.1337.ma>
2025-06-23 17:44:16 +0100

    an update

--- parsing/parsing.h
+++ parsing/parsing.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
+/*   parsing1.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/15 16:49:59 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/06/21 15:40:35 by haitaabe         ###   ########.fr       */
+/*   Created: 2025/06/23 17:43:11 by haitaabe          #+#    #+#             */
+/*   Updated: 2025/06/23 17:43:14 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 #include <errno.h>
 #include "../libft/libft.h"
 
+#define MAX_TOKENS 1024
+
 typedef enum e_token_type
 {
     PIPE,
@@ -29,13 +31,6 @@ typedef enum e_token_type
     WORD
 } t_token_type;
 
-typedef struct s_token
-{
-    char *value;          // The actual word or symbol (like "|", "<", ">>")
-    t_token_type type;    // The token type
-    struct s_token *next; // For linked list of tokens
-} t_token;
-
 typedef struct s_cmd
 {
     char **args;
@@ -43,34 +38,51 @@ typedef struct s_cmd
     char *outfile;
     int append;
     int heredoc;
-    int pipe_to_next; // 0 or 1
+    int pipe_to_next;
     struct s_cmd *next;
 } t_cmd;
 
-t_token *tokenize_input(char *line);
-// Adds a new token to the linked list
-void add_token(t_token **head, int type, char *value);
+typedef struct s_token
+{
+    t_token_type type;
+    char *value;
+    struct s_token *next;
+} t_token;
 
-// Returns 1 if character is |, <, >; 0 otherwise
-int is_special_char(char c);
-
-// to check spaces
-int is_space(int check_s);
-
-// Extracts a normal word (not in quotes)
-char *get_word(char *line, int *i);
-
-// just to use it instead of ft_strdup cuz we need the len of the word
-char *ft_strdup_range(char *str, int start, int len);
-// Extracts quoted word (supports single and double quotes)
-char *get_quoted_word(char *line, int *i);
-
-// Gets the token type for <, >, <<, >>
-int get_token_type(char *line, int *i);
-
-t_cmd *parse_tokens(t_token *token_list);
-int tokens(char *line);
-t_cmd *create_cmd(void);
-void add_arg_to_cmd(t_cmd *cmd, char *arg);
+t_cmd *parse_tokens(char **tokens);
+t_token_type get_token_type(char *str);
+t_cmd *new_cmd_node(void);
+void add_arg(char ***args, char *new_arg);
 void add_cmd_to_list(t_cmd **list, t_cmd *new_cmd);
+char **tokenize(char *line);
+int is_space(int check_s);
+int is_special(char c);
+void skip_spaces(char *line, int *i);
+int get_token_len(char *line, int i);
+int ft_strcmp(const char *s1, const char *s2);
+t_cmd *parse_input(char *input);
+char *extract_word(const char *input, int *i);
+char *extract_special(const char *input, int *i);
+char *extract_quoted(const char *input, int *i);
+void add_arg_to_cmd(t_cmd *cmd, char *arg);
+void set_infile(t_cmd *cmd, char *redir, char *file);
+void set_outfile(t_cmd *cmd, char *redir, char *file);
+char **tokenizer(const char *line);
+char *get_word(const char *line, int *i);
+char *get_quoted_word(const char *line, int *i);
+char *get_special_token(const char *line, int *i);
+void handle_redir(t_cmd *cmd, char **tokens, int *i);
+t_token *tokenize_input(char *str);
+void free_tokens(char **tokens);
+void free_cmds(t_cmd *head);
+void free_command_list(t_cmd *cmds);
+
+int check_syntax_error(char **tokens);
+
+char *expand_variables(const char *input, char **envp);
+
+int is_valid_identifier(const char *str);
+
+void print_syntax_error(const char *msg);
+
 #endif

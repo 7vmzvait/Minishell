07b2985803596596957db4592626fe53b0980a5b
Hamza Aitaabella <haitaabe@e1r1p4.1337.ma>
2025-06-28 17:12:32 +0100

    an update on syntax error

--- parsing/check_syntax_error.c
+++ parsing/check_syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 16:03:44 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/06/26 16:03:50 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/06/28 17:12:09 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,35 @@ int check_syntax_error(char **tokens)
         return 1;
 
     if (ft_strcmp(tokens[0], "|") == 0)
+    {
+        printf("syntax error near unexpected token `|'\n");
         return 1;
+    }
 
     while (tokens[i])
     {
-        if (ft_strcmp(tokens[i], "|") == 0 && (!tokens[i + 1] || ft_strcmp(tokens[i + 1], "|") == 0))
+        if (ft_strcmp(tokens[i], "|") == 0 &&
+            (!tokens[i + 1] || ft_strcmp(tokens[i + 1], "|") == 0))
+        {
+            printf("syntax error near unexpected token `|'\n");
             return 1;
-
+        }
         if ((ft_strcmp(tokens[i], "<") == 0 || ft_strcmp(tokens[i], ">") == 0 ||
-             ft_strcmp(tokens[i], "<<") == 0 || ft_strcmp(tokens[i], ">>") == 0) &&
-            (!tokens[i + 1] || is_special(tokens[i + 1][0])))
-            return 1;
-
+             ft_strcmp(tokens[i], "<<") == 0 || ft_strcmp(tokens[i], ">>") == 0))
+        {
+            if (!tokens[i + 1])
+            {
+                printf("syntax error near unexpected token `newline'\n");
+                return 1;
+            }
+            if (is_special(tokens[i + 1][0]))
+            {
+                printf("syntax error near unexpected token `%s'\n", tokens[i + 1]);
+                return 1;
+            }
+        }
         i++;
     }
 
-    return 0; // no syntax error
+    return 0;
 }

d746ee318cdc980bf9bded9f1ec0f26a3d36a1e2
El Mustapha Azmir <eazmir@c3r7p10.1337.ma>
2025-06-28 18:40:45 +0100

    Fix parsing of redirection and quotes in command input V7

--- parsing/check_syntax_error.c
+++ parsing/check_syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 16:03:44 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/06/28 17:12:09 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/06/26 16:03:50 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,22 +34,14 @@ int check_syntax_error(char **tokens)
     if (!tokens || !tokens[0])
         return 1;
 
-<<<<<<< HEAD
     if (!ft_strcmp(tokens[0], "|"))
     {
         print_error2("bash: syntax error near unexpected token","`|`");
         return (1);
-=======
-    if (ft_strcmp(tokens[0], "|") == 0)
-    {
-        printf("syntax error near unexpected token `|'\n");
-        return 1;
->>>>>>> 07b2985803596596957db4592626fe53b0980a5b
     }
 
     while (tokens[i])
     {
-<<<<<<< HEAD
         // if (!ft_strcmp(tokens[i], "|") && (!tokens[i + 1] || !ft_strcmp(tokens[i + 1], "|")))
         if (!ft_strcmp(tokens[i], "|") && tokens[i + 1] && !ft_strcmp(tokens[i + 1], "|"))
         {
@@ -69,30 +61,3 @@ int check_syntax_error(char **tokens)
 
     return (0); // no syntax error
 }
-=======
-        if (ft_strcmp(tokens[i], "|") == 0 &&
-            (!tokens[i + 1] || ft_strcmp(tokens[i + 1], "|") == 0))
-        {
-            printf("syntax error near unexpected token `|'\n");
-            return 1;
-        }
-        if ((ft_strcmp(tokens[i], "<") == 0 || ft_strcmp(tokens[i], ">") == 0 ||
-             ft_strcmp(tokens[i], "<<") == 0 || ft_strcmp(tokens[i], ">>") == 0))
-        {
-            if (!tokens[i + 1])
-            {
-                printf("syntax error near unexpected token `newline'\n");
-                return 1;
-            }
-            if (is_special(tokens[i + 1][0]))
-            {
-                printf("syntax error near unexpected token `%s'\n", tokens[i + 1]);
-                return 1;
-            }
-        }
-        i++;
-    }
-
-    return 0;
-}
->>>>>>> 07b2985803596596957db4592626fe53b0980a5b

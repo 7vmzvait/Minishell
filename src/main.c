250fafeac132ca25c304f6902c44687d8d9b996d
El Mustapha Azmir <eazmir@c3r6p1.1337.ma>
2025-07-12 17:49:15 +0100

    fix:  segfoult

--- src/main.c
+++ src/main.c
@@ -31,7 +31,7 @@ void	ft_readline(t_cmd *cmd, t_shell *shell, t_context *ctx, t_env *env_var)
 		if (cmd)
 			free_cmds(cmd);
 		cmd = parse_input(input, env_var);
-		if (is_parent_only_builtin(cmd->args[0]) && cmd->pipe_to_next != 1 && !cmd->infile &&  !cmd->outfile)
+		if (cmd && cmd->args && cmd->args[0] && is_parent_only_builtin(cmd->args[0]) && cmd->pipe_to_next != 1 && !cmd->infile &&  !cmd->outfile)
 			run_builtins(cmd,shell,env_var,ctx);
 		else
 			execute_commands(cmd, ctx, shell, env_var);
